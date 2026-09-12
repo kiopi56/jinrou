89bcc5ac1adf74acce47081f0074dbeecb142365
kiopi56 <166974362+kiopi56@users.noreply.github.com>
2026-09-12 22:54:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jinrou-v4";
+const CACHE = "jinrou-v5";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
